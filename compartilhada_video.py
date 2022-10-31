60652201d7b32b4ae6d8d232aa788815676688ac
Perdeste <thiagoyoshimura@hotmail.com>
2022-10-31 11:53:03 -0300

    Fix compartilhada_video.py versão com gargalo de memória

--- compartilhada_video.py
+++ compartilhada_video.py
@@ -5,59 +5,63 @@ import threading
 from time import time
 
 
-'''Função que insere os frames prontos em um arquivo de vídeo, ele é chamado apenas uma vez'''
-def insert_frames(output_list: list, filename: str, video_size: tuple, fps: int):
+'''Função que insere os frames prontos em um arquivo de vídeo de forma ordenada, ele é chamado apenas uma vez'''
+def insert_frames(output_dict: list, filename: str, video_size: tuple, fps: int, frame_count: int):
     file_output_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'output', filename + '_parallel_tratado.avi')
     output = cv2.VideoWriter(file_output_path, cv2.VideoWriter_fourcc(*'MJPG'), fps, video_size, isColor=False)
-    while True:
-        if len(output_list) > 0:
-            frame = output_list.pop(0)
-            # Caso a flag seja atendida, então todos os frames ja foram processadas
-            if frame[1] < 0:
-                break
-            output.set(cv2.CAP_PROP_POS_FRAMES, frame[1])
-            output.write(frame[0])
+    position = 0
+    while position < frame_count:
+        if position in output_dict:
+            frame = output_dict.pop(position)
+            output.write(frame)
+            position += 1
     output.release()
+
+
+'''Função para adicionar um frame a uma thread'''
+def create_frame_process(video, output_dict, thread_list, position, video_size, mascara):
+    ret, frame = video.read()
+    if ret:
+        process = threading.Thread(target=filter_frame, args=(frame, output_dict, position, video_size, mascara))
+        process.start()
+        thread_list.append(process)
+        position += 1
+    return position
 
 
 '''Função para controlar o fluxo de execução'''
 def execute_controller(thread: int, filename: str, video, video_size: tuple, mascara: np.ndarray, fps: int) -> None:
-    output_list = []
+    output_dict = {}
+    thread_list = []
     try:
-        # Iniciar a thread que fara o controle da inserção dos frames no arquivo final
-        insert_thread = threading.Thread(target=insert_frames, args=(output_list, filename, video_size, fps))
+        # Iniciar a thread que fara o controle da inserção dos frames no arquivo final. Existe gargalo de memória
+        # TODO: Para não precisar ordenar a lista de frames, utilizamos um dicionario. Verificar se existe uma maneira mais eficiente de uso de memória.
+        frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+        insert_thread = threading.Thread(target=insert_frames, args=(output_dict, filename, video_size, fps, frame_count))
         insert_thread.start()
         position = 0
-        while True:
-            thread_list = []
-            # Execute a função de transformação nas n threads
-            for _ in range(0, thread):
-                ret, frame = video.read()
-                if ret:
-                    process = threading.Thread(target=filter_frame, args=(frame, output_list, position, video_size, mascara))
-                    process.start()
-                    thread_list.append(process)
-                    position += 1
-            # Espera todas as n threads terminarem
-            for process in thread_list:
-                process.join()
-            if not ret:
-                # Adiciona o flag -1 para indicar quando acabar o processamento de todas as imagens
-                output_list.append((None,-1))
-                break
+        # Loop para executar n threads
+        for _ in range(0, thread):
+            position = create_frame_process(video, output_dict, thread_list, position, video_size, mascara)
+        # Loop para controlar o encerramento do processamento de cada thread, quando a thread terminar de executar ela ja coloca outro frame para processar (de forma ordenada)
+        while len(thread_list) > 0:
+            process = thread_list.pop(0)
+            process.join()
+            position = create_frame_process(video, output_dict, thread_list, position, video_size, mascara)
+        # Espera a thread de inserir terminar para finalizar a execução
         insert_thread.join()
     finally:
         video.release()
 
 
 '''Função executada por n threads, no qual aplica-se as 3 transformações na imagem'''
-def filter_frame(frame: np.ndarray, output_list: list, position: int, video_size: tuple, mascara: np.ndarray) -> None:
+def filter_frame(frame: np.ndarray, output_dict: list, position: int, video_size: tuple, mascara: np.ndarray) -> None:
     frame_input = cv2.resize(frame, video_size, fx = 0, fy = 0,
                             interpolation=cv2.INTER_CUBIC)   
     frame_input = cv2.cvtColor(frame_input, cv2.COLOR_BGR2GRAY)
     frame_output = cv2.filter2D(frame_input, ddepth=-1, kernel=mascara)
     # A primeira posição da tupla é o frame e a segunda a sua posição corresponde no vídeo
-    output_list.append((frame_output, position))
+    output_dict[position] = frame_output
 
 
 '''Função para ser chamada na main'''
